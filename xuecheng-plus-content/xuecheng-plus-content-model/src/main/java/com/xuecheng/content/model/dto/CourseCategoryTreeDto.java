aae8177149e169e508d533a20bcb91d9c67f4ecc
zhoujin <zhoujin_asi@163.com>
2025-07-01 23:54:43 +0800

    创建xuecheng-plus-base

--- xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/CourseCategoryTreeDto.java
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/CourseCategoryTreeDto.java
@@ -2,13 +2,14 @@ package com.xuecheng.content.model.dto;
 
 import com.xuecheng.content.model.po.CourseCategory;
 import lombok.Data;
+import lombok.ToString;
 
 import java.io.Serializable;
 import java.util.List;
 
 
 @Data
-
+@ToString(callSuper = true)
 public class CourseCategoryTreeDto extends CourseCategory implements Serializable {
     List<CourseCategoryTreeDto> childrenTreeNodes;
 
